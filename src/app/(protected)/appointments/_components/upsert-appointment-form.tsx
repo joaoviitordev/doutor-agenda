a45c0288f16265f740205d02773e534299053eb9
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2026-04-08 16:24:34 -0300

    feat: ensure only doctor available days are selectable on date select

--- src/app/(protected)/appointments/_components/upsert-appointment-form.tsx
+++ src/app/(protected)/appointments/_components/upsert-appointment-form.tsx
@@ -178,6 +178,19 @@ const UpsertAppointmentForm = ({
     });
   };
 
+  const isDateAvailable = (date: Date) => {
+    if (!selectedDoctorId) return false;
+    const selectedDoctor = doctors.find(
+      (doctor) => doctor.id === selectedDoctorId,
+    );
+    if (!selectedDoctor) return false;
+    const dayOfWeek = date.getDay();
+    return (
+      dayOfWeek >= selectedDoctor?.availableFromWeekDay &&
+      dayOfWeek <= selectedDoctor?.availableToWeekDay
+    );
+  };
+
   const isDateTimeEnabled = selectedPatientId && selectedDoctorId;
 
   return (
@@ -308,7 +321,7 @@ const UpsertAppointmentForm = ({
                       selected={field.value}
                       onSelect={field.onChange}
                       disabled={(date) =>
-                        date < new Date() || date < new Date("1900-01-01")
+                        date < new Date() || !isDateAvailable(date)
                       }
                       autoFocus
                     />
