37dffbca7e9bf7fdeea0bb69bec8fb1f222b725d
joaoviitordev <joaovitorgomesdefaria@gmail.com>
2026-04-08 10:03:32 -0300

    feat: add patients table

--- src/app/(protected)/patients/page.tsx
+++ src/app/(protected)/patients/page.tsx
@@ -14,9 +14,9 @@ import {
 import { db } from "@/db";
 import { patientsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
-
+import { patientsTableColumns } from "./_components/table-columns";
 import AddPatientButton from "./_components/add-patient-button";
-import PatientCard from "./_components/patient-card";
+import { DataTable } from "@/components/ui/data-table";
 
 const PatientsPage = async () => {
   const session = await auth.api.getSession({
@@ -45,11 +45,7 @@ const PatientsPage = async () => {
         </PageActions>
       </PageHeader>
       <PageContent>
-        <div className="grid grid-cols-3 gap-6">
-          {patients.map((patient) => (
-            <PatientCard key={patient.id} patient={patient} />
-          ))}
-        </div>
+      <DataTable data={patients} columns={patientsTableColumns} />
       </PageContent>
     </PageContainer>
   );
